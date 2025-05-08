342e6d8465e7aceb6d430f3d77d769e89c229ca8
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2025-05-08 18:37:30 -0300

    metodos estaticos

--- JavaScript - Node, Npm, Express, MongoDB/express/server.js
+++ JavaScript - Node, Npm, Express, MongoDB/express/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const routes = require('./routes.js');
+const path = require('path');
 const app = express();
 
+app.use(express.static(path.resolve(__dirname, 'public')));
 app.use(routes);
 
 app.listen(3000, () => {
