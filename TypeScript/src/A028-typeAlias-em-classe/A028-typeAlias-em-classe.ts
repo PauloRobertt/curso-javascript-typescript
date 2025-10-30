b6c59074a68be9af2b098a3a27879b01242fba11
paulorobertt <paulorobert5776@gmail.com>
2025-10-29 21:34:15 -0300

    Implementação type Alias em classes

---
+++ TypeScript/src/A028-typeAlias-em-classe/A028-typeAlias-em-classe.ts
@@ -0,0 +1,19 @@
+type TipoNome = {
+    nome:string;
+}
+
+type TipoSobrenome = {
+    sobrenome:string;
+}
+
+type TipoNomeCompleto = {
+    nomeCompleto():string;
+}
+
+class Pessoa implements TipoNome,TipoSobrenome,TipoNomeCompleto{
+    constructor(public nome:string, public sobrenome:string){}
+
+    nomeCompleto(): string {
+        return this.nome + ' ' + this.sobrenome;
+    }
+}
