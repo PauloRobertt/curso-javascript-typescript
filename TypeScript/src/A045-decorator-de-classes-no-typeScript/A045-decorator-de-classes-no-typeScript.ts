03dc6181c189ffb638e31892fd601905476fae83
paulorobertt <paulorobert5776@gmail.com>
2025-11-25 21:23:30 -0300

    Decorator de classes no TypeScript

---
+++ TypeScript/src/A045-decorator-de-classes-no-typeScript/A045-decorator-de-classes-no-typeScript.ts
@@ -0,0 +1,19 @@
+@decorator
+class Animal{
+    constructor(public cor:string){}
+}
+
+function decorator<T extends new (...args: any[]) => any> (target: T):T {
+    return new class extends target{
+        cor:string;
+
+        constructor(...args: any[]){
+            super(...args)
+            console.log('Teste');
+            this.cor = args[0];
+        }
+    }
+}
+
+const animal = new Animal('Preto');
+console.log('Decorator', animal)
