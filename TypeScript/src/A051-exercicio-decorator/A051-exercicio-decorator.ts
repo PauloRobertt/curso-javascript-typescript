89fc9ed06f5999a3b591abc849f54ab89983cc06
paulorobertt <paulorobert5776@gmail.com>
2025-12-01 22:55:44 -0300

    Decorator IsEmail do exercicio

--- TypeScript/src/A051-exercicio-decorator/A051-exercicio-decorator.ts
+++ TypeScript/src/A051-exercicio-decorator/A051-exercicio-decorator.ts
@@ -1,3 +1,25 @@
+import validator from 'validator';
+function IsEmail(target: any, propertyKey: string): any {
+  let valueEmail = target[propertyKey];
+  console.log('Valor Email', valueEmail);
+  Object.defineProperty(target, propertyKey, {
+    get() {
+      return valueEmail;
+    },
+    set(email: any) {
+      if (validator.isEmail(email)) {
+        valueEmail = email;
+        return;
+      }
+
+      console.log(`${email} não é um e-mail valido!`);
+      return;
+    },
+    enumerable: true,
+    configurable: true,
+  });
+}
+
 function MinLength(quantidadeDeCaracteres: number) {
   return function (target: any, propertyKey: string): any {
     let valorPropriedade = target[propertyKey];
@@ -29,6 +51,7 @@ class Exemplo {
   @MinLength(3)
   nome: string;
 
+  @IsEmail
   email: string;
 
   constructor(nome: string, email: string) {
