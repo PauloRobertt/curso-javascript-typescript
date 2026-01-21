64a549e23577f003d2a123a73464700dc5b0c4c9
paulorobertt <paulorobert5776@gmail.com>
2026-01-21 18:08:10 -0300

    Teste utilizando spyOn

--- Teste com jest/src/services/message.spec.ts
+++ Teste com jest/src/services/message.spec.ts
@@ -1,6 +1,24 @@
-describe('Testando describe', () => {
-  test('descrição do teste (test)', () => {
-    const number = 1;
-    expect(number).toBe(1);
+import { Message } from './message';
+
+describe('Message', () => {
+  it('Should return undefined', () => {
+    const sut = new Message();
+    expect(sut.sendMessage('Teste2')).toBeUndefined();
+  });
+
+  it('Testando message', () => {
+    // System under test
+    const sut = new Message();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.sendMessage('Teste');
+    expect(consoleSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('Testando message', () => {
+    // System under test
+    const sut = new Message();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.sendMessage('Teste');
+    expect(consoleSpy).toHaveBeenCalledWith('Teste');
   });
 });
